9f506d442667cd0afd7a1d535cc39583fc3aea33
saurabhxcod <offical.saurabhsingh@gmail.com>
2026-03-01 17:10:24 +0530

    addded readme

--- make_img.py
+++
@@ -1,4 +0,0 @@
-import base64
-png_data = b"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
-with open(r'c:\Users\kk\OneDrive - BENNETT UNIVERSITY\Desktop\campusConnect\test_banner.png', 'wb') as f:
-    f.write(base64.b64decode(png_data))
